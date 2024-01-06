a0990e6b736dc66fd9a637e72643ad454ad8d257
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-06 17:37:49 +0900

    commit change

--- src/index.js
+++ src/index.js
@@ -3,29 +3,32 @@ const {Client} = require("@notionhq/client");
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const retrievePage = async (databaseId) => {
-    return await notion.databases.query({database_id: databaseId});
-}
 
-const retrievePageProperties = async (pageId) => {
-    return await notion.pages.retrieve({page_id: pageId});
-}
-
-const retrievePageBlocks = async (pageId) => {
-    const block = await notion.blocks.children.list({ block_id: pageId });
-    for (const obj of block.results) {
-        if (obj.has_children) {
-            obj.children = await retrievePageBlocks(obj.id);
-        }
-    }
-    return block;
-}
 
 async function run() {
     try {
         const notion = new Client({
             auth: core.getInput("NOTION_TOKEN"),
         });
+
+        const retrievePage = async (databaseId) => {
+            return await notion.databases.query({database_id: databaseId});
+        }
+
+        const retrievePageProperties = async (pageId) => {
+            return await notion.pages.retrieve({page_id: pageId});
+        }
+
+        const retrievePageBlocks = async (pageId) => {
+            const block = await notion.blocks.children.list({ block_id: pageId });
+            for (const obj of block.results) {
+                if (obj.has_children) {
+                    obj.children = await retrievePageBlocks(obj.id);
+                }
+            }
+            return block;
+        }
+
         const databaseId = core.getInput("NOTION_DATABASE_ID");
         const pages = await retrievePage(databaseId);
         let results = [];
