0e74a99d886460c8907293a54322ab23284afb3a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-06 21:20:24 +0900

    release

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { writeFileSync } = require("fs");
-const { Client } = require("@notionhq/client");
+const {writeFileSync} = require("fs");
+const {Client} = require("@notionhq/client");
 const core = require("@actions/core");
 const github = require("@actions/github");
 
@@ -10,11 +10,11 @@ async function run() {
         });
 
         const retrievePageProperties = async (pageId) => {
-            return await notion.pages.retrieve({ page_id: pageId });
+            return await notion.pages.retrieve({page_id: pageId});
         };
 
         const retrievePageBlocks = async (pageId) => {
-            const block = await notion.blocks.children.list({ block_id: pageId });
+            const block = await notion.blocks.children.list({block_id: pageId});
             for (const obj of block.results) {
                 if (obj.has_children) {
                     obj.children = await retrievePageBlocks(obj.id);
@@ -24,71 +24,52 @@ async function run() {
         };
 
         const databaseId = core.getInput("NOTION_DATABASE_ID");
-        const pages = await notion.databases.query({ database_id: databaseId });
+        const pages = await notion.databases.query({database_id: databaseId});
 
         const pagePromises = pages.results.map(async (page) => {
             const pageProperties = await retrievePageProperties(page.id);
             pageProperties.blocks = await retrievePageBlocks(page.id);
-            const fileName = `${page.id}.json`;
+            const fileName = `content/${page.id}.json`;
             writeFileSync(fileName, JSON.stringify(pageProperties, null, 2));
-            core.info(`Wrote ${fileName}`);
-            return { fileName, pageProperties };
+            core.info(`Wrote ${fileName.split("/")[1]}`);
+            return {fileName, pageProperties};
         });
 
         // Wait for all pages to be processed and files to be written
         const results = await Promise.all(pagePromises);
 
-        // Commit and push for each file
-        const myToken = core.getInput("GITHUB_TOKEN");
-        const { owner, repo } = github.context.repo;
-        const branch = core.getInput("COMMIT_BRANCH");
-        const octokit = github.getOctokit(myToken);
+        // Commit all files to GitHub
+        const token = core.getInput("GITHUB_TOKEN");
+        const octokit = github.getOctokit(token);
 
-        const commitPromises = results.map(async ({ fileName, pageProperties }) => {
-            const file = fileName;
-            const content = Buffer.from(JSON.stringify(pageProperties, null, 2)).toString("base64");
-            const message = `Update ${fileName}`;
+        const commitMessage = "Update Notion database";
 
-            core.info(`Committing ${fileName} to ${owner}/${repo}@${branch}`);
-
-
-            const response = await octokit.rest.repos.getContent({
-                owner,
-                repo,
-                path: file
-            }).then((response) => {
-                if (response.status === 200) {
-                    octokit.rest.repos.createOrUpdateFileContents({
-                        owner,
-                        repo,
-                        path: file,
-                        message,
-                        content,
-                        branch,
-                        sha: response.data.sha,
-                    });
-                }
-
-                core.info(`Response: ${response.status}`);
-            }).catch((error) => {
-                // create new file
-                if (error.status === 404) {
-                    core.info(`Error: ${error.status}`);
-                    core.info(`Creating new file: ${file}`);
-                    octokit.rest.repos.createOrUpdateFileContents({
-                        owner,
-                        repo,
-                        path: file,
-                        message,
-                        content,
-                        branch,
-                    });
-                }
+        await octokit.git.createTree({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            base_tree: github.context.sha,
+            tree: results.map((result) => ({
+                path: result.fileName,
+                mode: "100644",
+                type: "blob",
+                content: JSON.stringify(result.pageProperties),
+            })),
+        }).then(async (tree) => {
+            await octokit.git.createCommit({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                message: commitMessage,
+                tree: tree.data.sha,
+                parents: [github.context.sha],
+            }).then(async (commit) => {
+                await octokit.git.updateRef({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    ref: "heads/master",
+                    sha: commit.data.sha,
+                });
             });
         });
-
-        // Wait for all commits to be completed
-        await Promise.all(commitPromises);
     } catch (error) {
         core.setFailed(error);
     }
